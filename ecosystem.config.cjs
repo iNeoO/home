eaf61787d7e6ceae5143b0d1739bb42373ab100b
Valère Pique <valere.pique@sg.social.gouv.fr>
2026-03-26 17:37:03 +0100

    feat: small update on config for deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'home',
-      port: '3002',
       exec_mode: 'fork',
       instances: 1,
       script: './.output/server/index.mjs',
+      env: {
+        PORT: 3002,
+        HOST: '127.0.0.1',
+        NODE_ENV: 'production',
+      },
     },
   ],
 };
