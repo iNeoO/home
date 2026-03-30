5ade5f2e875ee4b6365d4175b6fad21bb1f0ec97
Valère Pique <valere.pique@sg.social.gouv.fr>
2026-03-30 11:34:53 +0200

    feat: change default port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       instances: 1,
       script: './.output/server/index.mjs',
       env: {
-        PORT: 3002,
+        PORT: 3000,
         HOST: '127.0.0.1',
         NODE_ENV: 'production',
       },
